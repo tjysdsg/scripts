33dc547490dfefc96610ab1ce57156a220fdb167
tjysdsg <tjy1018543509@163.com>
2021-12-22 06:46:07 +0800

    Update

---
+++ adobe_killer.bat
@@ -0,0 +1,44 @@
+:--------------------------------------
+:Developed by :- krishna247india
+:Download URL :- https://github.com/krishna247india/AdobeKiller.git
+:--------------------------------------
+@echo off
+net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo.
+    ) else (
+        GOTO :NoAdmin
+    )
+TASKKILL /F /IM "Creative Cloud.exe" /T
+TASKKILL /F /IM "Adobe Desktop Service.exe" /T
+TASKKILL /F /IM "Adobe CEF Helper.exe" /T
+TASKKILL /F /IM "Adobe Installer.exe" /T
+TASKKILL /F /IM "AdobeUpdateService.exe" /T
+TASKKILL /F /IM "CoreSync.exe" /T
+TASKKILL /F /IM "AdobeIPCBroker.exe" /T
+TASKKILL /F /IM "Adobe Installer.exe" /T
+TASKKILL /F /IM "CCLibrary.exe" /T
+TASKKILL /F /IM "AdobeNotificationClient.exe" /T
+TASKKILL /F /IM "RuntimeBroker.exe" /T
+TASKKILL /F /IM "AdobeIPCBroker.exe" /T
+TASKKILL /F /IM "AdobeUpdateService.exe" /T
+TASKKILL /F /IM "CCXProcess.exe" /T
+TASKKILL /F /IM "backgroundTaskHost.exe" /T
+TASKKILL /F /IM "AGSService.exe" /T
+TASKKILL /F /IM "AGMService.exe" /T
+TASKKILL /F /IM "LogTransport2.exe" /T
+TASKKILL /F /IM "AdobeGCClient.exe" /T
+ECHO|SET /p =" -       Press any key to exit ..."
+Pause >NUL
+goto :eof
+:NoAdmin
+ECHO.
+ECHO"  - You must run this script in Administrator mode"
+ECHO.
+ECHO.
+ECHO.
+ECHO|SET /p =" -       Press any key to exit ..."
+Pause >NUL
+ECHO   GoodBye
+ECHO.
+ECHO.
--- gpa_calc.py
+++ gpa_calc.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-scores = [3, 4, 2.7, 3.7, 4, 4, 3.7, 3.7, 4, 3.7, 3.3, 4]
+# scores = [3, 4, 2.7, 3.7, 4, 4, 3.7, 3.7, 4, 3.7, 3.3, 4, 4, 4]  # major gpa
+scores = [4, 3.3, 4, 3.7, 4, 4, 3.7, 3.7, 3.7, 3.7, 4, 3.7, 3.3, 4, 4, 4]  # last two years
 credits = [4 for _ in range(len(scores))]
 
 scores = np.asarray(scores)
